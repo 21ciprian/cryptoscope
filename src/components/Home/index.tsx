e7e5b5fd5a791e64372857356be82f3460b97954
21ciprian <2021ciprianoffice@gmail.com>
2022-03-09 01:07:32 +0000

    Added content to footer component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,33 @@
+import {Col, Row, Statistic, Typography} from 'antd'
 import React from 'react'
+const {Title} = Typography
 
-type Props = {}
-
-function Home({}: Props) {
-	return <div>Home</div>
+function Home() {
+	return (
+		<>
+			<Title level={2} className='title__heading'>
+				Crypto Stats
+			</Title>
+			<Row>
+				<Col span={12}>
+					<Statistic title='Total CryptoCurrencies' value='5' />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total Exchanges' value='5' />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total Market Cap' value='5' />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total 24h Volume' value='5' />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total Markets' value='5' />
+				</Col>
+				<Col span={12}></Col>
+			</Row>
+		</>
+	)
 }
 
 export default Home
