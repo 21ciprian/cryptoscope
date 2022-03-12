46b518a53555ef54a50080fce855f072a4ec3817
21ciprian <2021ciprianoffice@gmail.com>
2022-03-12 20:03:11 +0000

    Created news cards

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -1,9 +1,38 @@
+import {Card, Col, Row, Typography} from 'antd'
 import React from 'react'
+import {useGetCryptoNewsQuery} from '../../services/cryptoNewsApi'
+const {Title} = Typography
+type Props = {
+	data: {
+		_type: string
+		name: string
+	}
+}
 
-type Props = {}
-
-function News({}: Props) {
-	return <div>News</div>
+function News() {
+	const {data: cryptoNews} = useGetCryptoNewsQuery({
+		newsCategory: 'Cryptocurrency',
+		count: 10,
+	})
+	console.log('cryptoNews: ', cryptoNews)
+	if (!cryptoNews?.value) return <h2>Loading</h2>
+	return (
+		<Row gutter={[24, 24]}>
+			{cryptoNews.value.map((news, i) => (
+				<Col xs={24} sm={12} lg={8} key={i}>
+					<Card hoverable className='news-card'>
+						<a href={news.url} target='_blank' rel='noreferrer'>
+							<div className='news-image-container'>
+								<Title className='news-titel' level={4}>
+									{news.name}
+								</Title>
+							</div>
+						</a>
+					</Card>
+				</Col>
+			))}
+		</Row>
+	)
 }
 
 export default News
--- src/services/cryptoNewsApi.ts
+++ src/services/cryptoNewsApi.ts
@@ -1,16 +1,21 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import {Coin} from '../components/Cryptocurrencies'
 
-export type Query = {
-	data: {
-		stats: any
-		coins: Coin[]
-		status: string
+export type Value = {
+	url: string
+	_type: string
+	name: string
+	datePublished: string
+	description: string
+	image: {
+		thumbnail: {
+			contentUrl: string
+		}
 	}
 }
 export type Category = {
 	newsCategory: string
 	count: number
+	value: Value[]
 }
 const cryptoNewsHeaders = {
 	'x-bingapis-sdk': 'true',
@@ -23,7 +28,7 @@ export const cryptoNewsApi = createApi({
 	reducerPath: 'cryptoNewsApi',
 	baseQuery: fetchBaseQuery({baseUrl}),
 	endpoints: builder => ({
-		getCryptoNews: builder.query<Category, Category>({
+		getCryptoNews: builder.query<Category, Category | undefined | any>({
 			query: ({newsCategory, count}) =>
 				createRequest(
 					`/news/search?=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
